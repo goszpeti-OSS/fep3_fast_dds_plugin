1dfe7ce0ba033f145550df1b70b15f3d1aa6d20e
goszpeti <peter.gosztolya@gmail.com>
2023-05-14 23:58:15 +0200

    Use boost 1.75 in fep3 participant

--- thirdparty/fep3_participant/conanfile.py
+++ thirdparty/fep3_participant/conanfile.py
@@ -34,7 +34,7 @@ class ConanProduct(ConanFile):
 
     def requirements(self):
         self.requires("dev_essential/1.2.0")
-        self.requires("boost/1.81.0")
+        self.requires("boost/1.75.0")
 
     def build(self):
         patch_str = (Path(self.source_folder) / "patch1.diff").read_text()
